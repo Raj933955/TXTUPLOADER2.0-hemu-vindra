0dbf71302d3d10c85635dae842b1919a812475f5
Raj933955 <162988577+Raj933955@users.noreply.github.com>
2024-04-15 12:47:04 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
-api_id = "25298674"
-api_hash = "1a7e01278b2bf0fe975fb8ce7c5ead8d"
-bot_token = "7092396095:AAF_jP7qZx_cC0esduKkPafHGt9vhsrWxiM"
+api_id = "27952989"
+api_hash = "74f04808a359e9a516e955ec243613ca"
+bot_token = "7146879962:AAF4qVFr6GpyJ2cigiCwrdUwRfY5_cvYLBI"
